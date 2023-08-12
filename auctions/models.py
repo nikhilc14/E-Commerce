d4a2d7eabdea08df163cc97074589e99a6b764b9
nikhilc14 <nikhilchoudhary.official@gmail.com>
2023-08-12 20:26:15 +0530

    Intial models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class auction_listings(models.Model):
+    name = models.CharField(max_length=256)
+    price = models.FloatField(default=0.0)
+    created_on = models.DateTimeField()
+
+class bids(models.Model):
+    pass
